c54027364e84d752ea812f1b38c1634d5873e301
Ryan Davis <ryan@mokeys.org>
2010-10-30 17:18:22 -0400

    got the total number of rolls and last roll showing

--- catan.js
+++ catan.js
@@ -1,15 +1,30 @@
 $(function(){
       var nums = $('#numbers');
+      var rollDisplay = $('#rolls');
+      var lastRoll = $('#last-roll');
       var rolls = 0;
+      var rollHistory = [];
+      var rollHistogram = {};
+      var updateStats = function(die){
+
+	  rollHistory.push(die);
+	  rollDisplay.text(rollHistory.length);
+	  rollHistogram[die]++;
+	  lastRoll.text(die);
+      };
       for(var i =1; i<=12; i++){
+	  rollHistogram[i] = 0; //initialize the histogram
 	  var btn = $('<button>');
 	  btn.append($("<strong>").append(i));
 	  btn.append($("<div>").addClass("count").append("0"));
-	  btn.click(function(){
-			rolls += 1;
-			var cnt = parseInt($('.count', this).text());
-			$('.count', this).text(cnt+1);
-		    });
+	  (function(die){
+	       btn.click(function(){
+			     var cnt = parseInt($('.count', this).text());
+			     $('.count', this).text(cnt+1);
+			     updateStats(die);
+			 });
+	       
+	  })(i); //jump through hoops to close in the die value
 	  nums.append(btn);
       }
   });
