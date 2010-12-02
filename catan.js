1fb0751d2f558c103674822a743b37b16256a498
Ryan Davis <ryan@mokeys.org>
2010-12-01 19:15:30 -0500

    added some html5 localstorage so the game persists

--- catan.js
+++ catan.js
@@ -15,29 +15,48 @@ $(function(){
       var nums = $('#numbers');
       var rollDisplay = $('#rolls');
       var lastRoll = $('#last-roll');
-      var rolls = 0;
+
+
       var rollHistory = [];
       var rollHistogram = {};
-      var updateBoard = function(die){
-	  rollHistory.push(die);
-	  rollDisplay.text(rollHistory.length);
-	  rollHistogram[die]++;
-	  lastRoll.text(die);
+    
+      try{
+	  rollHistory = $.parseJSON(localStorage.rollHistory) || [];
+	  rollHistogram = $.parseJSON(localStorage.rollHistogram) || {};    
+      } catch (x) { }
+
+      var rolls = rollHistory.length;
+
+      var updateEVs = function(){
 	  $('button').each(function(idx, elem){
 			       var btn = $(elem);
 			       var roll = parseInt(btn.data('roll'));
 			       var ev = rollHistory.length * probs[roll];
 			       console.log(roll, probs[roll], ev, rollHistory.length);
-			       
-			       $('.ev', elem).text(ev.toString().substring(0,5));
+			       $('.ev', btn).text(ev.toString().substring(0,5));
 			   });
+
+	  
+      };
+      var updateStats = function(){
+	  lastRoll.text(rollHistory[rollHistory.length-1]);
+	  rollDisplay.text(rollHistory.length);	  
+      };
+      var updateBoard = function(die){
+	  rollHistory.push(die);
+	  rollHistogram[die]++;
+	  updateStats();
+	  updateEVs();
+	  localStorage.rollHistory = $.toJSON(rollHistory);
+	  localStorage.rollHistogram = $.toJSON(rollHistogram);
       };
       for(var i =2; i<=12; i++){
-	  rollHistogram[i] = 0; //initialize the histogram
+	  if(rollHistogram[i] === undefined)
+	      rollHistogram[i] = 0; //initialize the histogram
 	  var btn = $('<button>');
 	  btn.append($("<strong>").append(i));
 	  btn.append($('<br>'));
-	  btn.append($("<span>").addClass("count").append("0"));
+	  btn.append($("<span>").addClass("count").append(rollHistogram[i]));
 	  btn.append($("<span>").addClass("ev").append("0"));
 	  btn.data('roll', i);
 	  btn.click(function(){
@@ -47,4 +66,13 @@ $(function(){
 		    });
 	  nums.append(btn);
       }
+      updateEVs();
+      updateStats();
+      $('#new-game').click(function(){
+			      if(confirm('Start new game?')){
+				  delete localStorage.rollHistory;
+				  delete localStorage.rollHistogram;
+				  document.location = "/";
+			      } 
+			   });
   });
